8e1ef58638e5e05a4e5022cbfbdb3cb5b8613318
Pawar-Darshan <officialdarshan007@gmail.com>
2022-08-21 11:26:15 +0530

    Profile Pictures

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greeting
+        name="Darshan"
+        proPic="http://www.goodmorningimagesdownload.com/wp-content/uploads/2021/01/Best-Single-Boys-Whatsapp-Dp-Wallpaper.jpg"
+        intro="Full Stack Developer"
+      />
+      <Greeting
+        name="Surabhi"
+        proPic="https://2.bp.blogspot.com/-hM5hDWlwVCQ/W4_V25OFXnI/AAAAAAAAPKA/uTt3Ag6RedQ-3NFXws_h1uKfMZFSPdxWQCLcBGAs/s1600/cute-girls-dp-download.jpg"
+        intro="Doctor"
+      />
+      <Greeting
+        name="Prafulla"
+        proPic="https://wallpaperspeed.id/gallery/wallpaper/20220428/anime-boys-face-dp-whatsapp-wallpapers-wallpaper,best-anime-profile-pic-for-instagram-for-boy-preview.webp"
+        intro="Mechanical Engineer"
+      />
     </div>
   );
 }
 
+function Greeting({ name, proPic, intro }) {
+  return (
+    <div>
+      <h1>Hello, {name} here😍🥳👋</h1>
+      <img src={proPic} alt={name} />
+      <h3>I am a {intro} </h3>
+    </div>
+  );
+}
 export default App;
